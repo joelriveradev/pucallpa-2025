99bd3f7b8f5857f7c91349be2ec2c5933f4a49a7
joelriveradev <joelriveradev@gmail.com>
2025-02-03 08:14:19 -0600

    feat: add photo carousel

---
+++ src/components/photo-carousel.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselApi,
+} from '@/components/ui/carousel'
+
+import Image from 'next/image'
+
+interface Props {
+  photos: string[]
+}
+
+export function PhotoCarousel({ photos }: Props) {
+  const [current, setCurrent] = useState(0)
+  const [count, setCount] = useState(0)
+  const [api, setApi] = useState<CarouselApi>()
+
+  useEffect(() => {
+    if (!api) return
+
+    setCount(api.scrollSnapList().length)
+    setCurrent(api.selectedScrollSnap() + 1)
+
+    api.on('select', () => {
+      setCurrent(api.selectedScrollSnap() + 1)
+    })
+  })
+
+  return (
+    <div className='w-full mb-7'>
+      <Carousel setApi={setApi}>
+        <CarouselContent>
+          {photos.map((url) => {
+            return (
+              <CarouselItem key={url}>
+                <Image
+                  src={url}
+                  width={0}
+                  height={0}
+                  sizes='100vw'
+                  style={{ width: '100%', height: 'auto' }}
+                  alt=''
+                  className='rounded-xl mb-2'
+                />
+              </CarouselItem>
+            )
+          })}
+        </CarouselContent>
+      </Carousel>
+
+      <div className=' text-sm text-muted-foreground'>
+        Slide {current} of {count}
+      </div>
+    </div>
+  )
+}
