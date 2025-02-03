b10c48c4fdb7855c14fc63808cf5c50ab4c68628
joelriveradev <joelriveradev@gmail.com>
2025-02-03 08:17:34 -0600

    feat: add photos and videos in posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import { createClient } from '@/lib/supabase/server'
 import { Card } from '@/components/ui/card'
+import { PhotoCarousel } from '@/components/photo-carousel'
 import { formatDateCreated } from '@/lib/utils'
+import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { MessageSquare } from 'lucide-react'
+
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default async function HomePage() {
   const db = await createClient()
@@ -8,7 +15,7 @@ export default async function HomePage() {
 
   const { data: posts, error } = await db
     .from('posts')
-    .select('id, title, content, video_urls, photo_urls, created_at')
+    .select('id, title, content, photo_urls, video_urls, created_at')
     .order('created_at', { ascending: false })
     .eq('stage', 'PUBLISHED')
 
@@ -24,48 +31,81 @@ export default async function HomePage() {
   }
 
   return (
-    <main className='w-full min-h-dvh px-10 py-20 lg:px-20'>
+    <main className='w-full min-h-dvh px-10 py-20 lg:px-20 max-w-3xl mx-auto'>
       <h1 className='text-xl lg:text-2xl font-bold mb-2'>
         Pucallpa Mission 2025 🇵🇪
       </h1>
 
-      <p className='text-neutral-300'>
+      <p className='text-muted-foreground'>
         Sharing the love of Christ the people of Pucallpa.
       </p>
 
+      <Link
+        href='/subscribe'
+        prefetch
+        className={cn(
+          buttonVariants({ variant: 'outline', className: 'mt-5' })
+        )}
+      >
+        Subscribe for SMS updates! <MessageSquare size={20} />
+      </Link>
+
       <div className='mt-20'>
-        {posts.map(({ id, title, content, created_at, photo_urls }) => {
-          return (
-            <Card
-              key={id}
-              className='p-5 bg-neutral-900/30 max-w-lg -ml-5 mb-7'
-            >
-              <h2 className='font-semibold mb-1'>{title}</h2>
+        {posts.length === 0 ? (
+          <p className='text-muted-foreground'>
+            No posts to show yet. Check back later!
+          </p>
+        ) : null}
 
-              <small className='mb-7 block text-neutral-300 font-mono'>
-                {formatDateCreated(created_at)}
-              </small>
+        {posts.map(
+          ({ photo_urls, video_urls, content, created_at, id, title }) => {
+            return (
+              <Card
+                key={id}
+                className='p-5 bg-neutral-900/30 mb-7 pb-7 rounded-2xl'
+              >
+                <h2 className='font-semibold mb-1'>{title}</h2>
 
-              {photo_urls &&
-                photo_urls.map((url) => {
-                  const src = `${storageUrl}/${url}`
+                <small className='mb-7 block text-neutral-300 font-mono'>
+                  {formatDateCreated(created_at)}
+                </small>
 
-                  return (
-                    <img
-                      key={url}
-                      src={src}
-                      alt={title}
-                      className='w-full h-52 object-cover rounded-lg mb-5'
-                    />
-                  )
-                })}
+                {video_urls !== null && video_urls.length === 1 && (
+                  <video
+                    src={`${storageUrl}/${video_urls[0]}`}
+                    width={0}
+                    height={0}
+                    style={{ width: '100%', height: 'auto' }}
+                    className='rounded-xl mb-7'
+                    controls
+                  />
+                )}
 
-              <p className='antialised text-pretty text-neutral-300'>
-                {content}
-              </p>
-            </Card>
-          )
-        })}
+                {photo_urls !== null && photo_urls.length === 1 && (
+                  <Image
+                    src={`${storageUrl}/${photo_urls[0]}`}
+                    width={0}
+                    height={0}
+                    sizes='100vw'
+                    style={{ width: '100%', height: 'auto' }}
+                    alt=''
+                    className='rounded-xl mb-7'
+                  />
+                )}
+
+                {photo_urls !== null && photo_urls.length > 1 && (
+                  <PhotoCarousel
+                    photos={photo_urls.map((url) => `${storageUrl}/${url}`)}
+                  />
+                )}
+
+                <p className='antialised text-pretty text-neutral-300'>
+                  {content}
+                </p>
+              </Card>
+            )
+          }
+        )}
       </div>
     </main>
   )
